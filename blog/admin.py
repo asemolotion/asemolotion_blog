8e769a1e24be41a8da8f73c7e55821b2160c370e
広石財 <hiroishizai@takaras-mac.local>
2020-01-08 02:58:47 +0900

    Update Post and Tag model #8

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from .models import Post
+from .models import Post, Tag
 
+admin.site.register(Tag)
 admin.site.register(Post, MarkdownxModelAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,14 +2,27 @@ from django.db import models
 from markdownx.models import MarkdownxField
 from markdownx.utils import markdownify
 
+
+
+class Tag(models.Model):
+    """ 記事につけるタグクラス　"""
+    name = models.CharField('タグ名', max_length=255)
+    slug = models.SlugField('タグスラグ', unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     """
     Model definition for ブログの投稿.    
     """
     title = models.CharField('タイトル', max_length=255)
-    # content = models.TextField('本文')
+    description = models.CharField('説明', max_length=255, blank=True, default='', null=True)
     content = MarkdownxField('本文', help_text='write down in markdown format')
     
+    tags = models.ManyToManyField(Tag, blank=True)
+
     created_at = models.DateTimeField('作成日', auto_now_add=True)
     updated_at = models.DateTimeField('更新日', auto_now=True)    
 
