67a8344a5ae4b8f65ba87b057b9017e36ee7798c
広石財 <hiroishizai@takaras-mac.local>
2020-01-08 00:34:33 +0900

    markdownで書いて、表示できる。#1

--- conf/settings.py
+++ conf/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Add Media URL and Root
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # import variables from local settings.
 try:
--- conf/urls.py
+++ conf/urls.py
@@ -20,4 +20,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('general.urls')),
     path('blog/', include('blog.urls')),
+    
+    path('markdownx/', include('markdownx.urls')), 
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
