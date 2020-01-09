e63f80680b6366a0f93f7c50194c15f512759cfa
広石財 <hiroishizai@takaras-mac.local>
2020-01-09 15:39:22 +0900

    add About and Contact page #11

--- general/views.py
+++ general/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
+
+from blog.models import Post
+
 
 class IndexView(TemplateView):
     template_name = 'general/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        # Postのリスト
+        context['post_list'] = Post.objects.filter().order_by('-created_at')
+        
+        return context
+    
+
+
 class AboutView(TemplateView):
-    pass 
+    template_name = 'general/about.html'
 
 class ContactView(TemplateView):
-    pass
+    template_name = 'general/contact.html'
