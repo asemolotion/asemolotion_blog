1ea147b19f2522c0114fda8e47028e0cae07c3f4
広石財 <hiroishizai@takaras-mac.local>
2020-01-09 17:13:08 +0900

    add google analytics tag and update index page #11

--- general/views.py
+++ general/views.py
@@ -2,18 +2,25 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-from blog.models import Post
+from blog.models import Post, Project, Tag
 
 
 class IndexView(TemplateView):
     template_name = 'general/index.html'
 
+    POST_ITEM_COUNT = 7  # トップページで表示する記事リストのアイテム数
+    PROJECT_ITEM_COUNT = 4
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
         # Postのリスト
-        context['post_list'] = Post.objects.filter().order_by('-created_at')
+        context['post_list'] = Post.objects.filter().order_by('-created_at')[:self.POST_ITEM_COUNT]
         
+        # Projectのリスト
+        context['project_list'] = Project.objects.order_by('timestamp')[:self.PROJECT_ITEM_COUNT]
+
         return context
     
 
