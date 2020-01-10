045842f44295c230f33568993a268e5f12981bfe
広石財 <hiroishizai@takaras-mac.local>
2020-01-10 15:00:14 +0900

    BasePostListViewをつくって条件分岐を継承した#11

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from .views import *
 app_name = 'blog'
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='list'),
-    path('<int:pk>/', BlogDetailView.as_view(), name='detail'),
+    path('', PostListView.as_view(), name='list'),
+    path('<int:pk>/', PostDetailView.as_view(), name='detail'),
     path('project/<slug:project_slug>/', ProjectView.as_view(), name='project'),
     path('tag/<slug:tag_slug>/', TagView.as_view(), name='tag'),
 ]
