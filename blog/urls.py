bbede99aef5cec293d39eb05e62cdac920fd0585
広石財 <hiroishizai@takaras-mac.local>
2020-01-10 14:51:46 +0900

    projectページ、tagページ追加

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='list'),
-    path('<int:pk>/', BlogDetailView.as_view(), name='detail')
+    path('<int:pk>/', BlogDetailView.as_view(), name='detail'),
+    path('project/<slug:project_slug>/', ProjectView.as_view(), name='project'),
+    path('tag/<slug:tag_slug>/', TagView.as_view(), name='tag'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -26,3 +26,53 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/detail.html'
+
+
+class ProjectView(ListView):
+    template_name = 'blog/project.html'
+    slug = ''
+
+    def get_queryset(self):
+
+        if self.request.user.is_superuser:
+            queryset = Post.objects
+        
+        elif self.request.session.get('invitation_verification') == 'ok':
+            queryset = Post.objects.exclude(status='draft')
+
+        else:
+            queryset = Post.objects.exclude(status='draft').exclude(release_condition='limited')
+
+
+        self.slug = self.kwargs.get('project_slug')
+        queryset = queryset.filter(project__slug=self.slug)
+        
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        project = Project.objects.filter(slug=self.slug).first()
+        context['project'] = project
+
+        return context
+    
+
+
+class TagView(ListView):
+    template_name = 'blog/tag.html'
+    slug = ''
+
+    def get_queryset(self):
+
+        self.slug = self.kwargs.get('tag_slug')
+        queryset = Post.objects.filter(tags__slug=self.slug)  # M2Mの1個をだす条件のクエリ
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        tag = Tag.objects.filter(slug=self.slug).first()
+        context['tag'] = tag
+
+        return context
