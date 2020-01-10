b8d5bc9bc4b051faf0a03a5a1862486fb09d840b
広石財 <hiroishizai@takaras-mac.local>
2020-01-10 17:14:40 +0900

    highlight.jsを適用 #31

--- conf/settings.py
+++ conf/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Add Media URL and Root
 MEDIA_URL = '/media/'
