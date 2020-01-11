6e35b21d28960d226bb0183462034ea99dc7a076
広石財 <hiroishizai@takaras-mac.local>
2020-01-11 15:52:10 +0900

    adminでのtextareaを拡大した。レスポンシブでめんどくさい。#34

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+
+from markdownx.models import MarkdownxField
 from markdownx.admin import MarkdownxModelAdmin
+from markdownx.widgets import AdminMarkdownxWidget
+
 from .models import Post, Tag, Project, FileLink
 
 
@@ -7,7 +11,31 @@ admin.site.register(Tag)
 admin.site.register(Project)
 admin.site.register(FileLink)
 
+class CustomAdminMarkdownxWidget(AdminMarkdownxWidget):
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.attrs={'cols': 50, 'rows': 30}
+		"""
+		forms.TextAreaのattrsを書き換えている。
+		"""
+	
+
 class CustomMarkdownxModelAdmin(MarkdownxModelAdmin):
-    list_display = ('title', 'description', 'status', 'release_condition', 'post_type')
-    
+	list_display = ('title', 'description', 'status', 'release_condition', 'post_type')
+	
+	formfield_overrides = {
+		   MarkdownxField: {'widget': CustomAdminMarkdownxWidget}
+	} 
+   
+	# class Meta:
+	# 	widgets = {
+	# 		'text': MarkdownxWidget(attrs={
+	# 			# 'class': 'textarea',
+	# 			# 'style': 'height:400px;',
+	# 			# 'rows':50, 
+	# 			# 'cols':40
+	# 			}),
+	# 	}        
+
 admin.site.register(Post, CustomMarkdownxModelAdmin)
